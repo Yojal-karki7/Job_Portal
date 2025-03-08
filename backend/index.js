e14e78162171df1ccd592d883b333fbf4ad2a25a
Yojal-karki7 <yojalkarki@gmail.com>
2025-03-08 12:41:10 +0545

    complete application controller and api routes

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import connectDB from './utils/db.js';
 import userRoute from './routes/userRoutes.js'
 import companyRoute from './routes/companyRoutes.js'
 import jobRoute from './routes/jobRoutes.js'
+import applicationRoute from './routes/applicationRoutes.js'
 
 dotenv.config({});
 
@@ -26,6 +27,7 @@ app.use(cors(corsOptions));
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/company', companyRoute);
 app.use('/api/v1/job', jobRoute);
+app.use('/api/v1/application', applicationRoute);
 
 app.listen(PORT, ()=>{
     connectDB()
---
+++ backend/routes/applicationRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import isAuthenticated from '../middlewares/auth.js';
+import { applyJob, getApplicants, getAppliedJobs, updateStatus } from '../controllers/applicationController.js';
+
+const router = express.Router();
+
+router.get("/getappliedjobs", isAuthenticated, getAppliedJobs)
+router.get('/apply/:id', isAuthenticated, applyJob)
+router.get('/:id/applicants', isAuthenticated, getApplicants)
+router.get('/status/:id/update', isAuthenticated, updateStatus)
+
+export default router
