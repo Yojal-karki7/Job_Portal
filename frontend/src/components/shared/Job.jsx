b3f03c31a5857f7a3c41309393b20042abb5c25e
Yojal-karki7 <yojalkarki@gmail.com>
2025-03-15 12:17:25 +0545

    Job description page

--- frontend/src/components/shared/Job.jsx
+++ frontend/src/components/shared/Job.jsx
@@ -3,8 +3,11 @@ import { Button } from "../ui/button";
 import { Bookmark } from "lucide-react";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Badge } from "../ui/badge";
+import { useNavigate } from "react-router-dom";
 
 const Job = () => {
+  const navigate = useNavigate();
+  const jobId = 'sjdasdkafjksagkaga'
   return (
     <div className="p-4 rounded-md shadow-xl bg-white border border-gray-100 w-full">
       <div className="flex items-center justify-between">
@@ -43,7 +46,7 @@ const Job = () => {
         </Badge>
       </div>
       <div className="flex items-center gap-4 mt-4">
-            <Button variant="outline">Details</Button>
+            <Button variant="outline" onClick={()=>navigate(`/description/${jobId}`)}>Details</Button>
             <Button className="bg-[#7209B7] sm:w-[55%]">Save for Later</Button>
         </div>
     </div>
