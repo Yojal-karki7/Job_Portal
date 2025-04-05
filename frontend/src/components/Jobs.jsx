10a4df1fa6a02a2dad9b8bcf1e1ab7ef6835c065
Yojal-karki7 <yojalkarki@gmail.com>
2025-04-05 09:11:24 +0545

    implement filter logic on jobs page

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./shared/Navbar";
 import FilterCard from "./shared/FilterCard";
 import Job from "./shared/Job";
 import { useSelector } from "react-redux";
 import useGetAllJobs from "@/hooks/useGetAllJobs";
 
-const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
+// const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const Jobs = () => {
-  useGetAllJobs()
-  const {allJobs } = useSelector(store => store.job)
+  const {allJobs, searchedQuery } = useSelector(store => store.job)
+  const [filterJobs, setFilterJobs] = useState(allJobs);
+
+  useEffect(()=>{
+    if(searchedQuery) {
+      const filteredJobs = allJobs.filter((job) => {
+        return job.title.toLowerCase().includes(searchedQuery.toLowerCase()) || 
+        job.description.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+        job.location.toLowerCase().includes(searchedQuery.toLowerCase())
+      })
+      setFilterJobs(filteredJobs)
+    } else {
+      setFilterJobs(allJobs)
+    }
+  }, [allJobs,searchedQuery])
   return (
     <div>
       <Navbar />
@@ -18,12 +31,12 @@ const Jobs = () => {
           <div className="w-[20%]">
             <FilterCard />
           </div>
-          {jobsArray.length <= 0 ? (
+          {filterJobs.length <= 0 ? (
             <span>Job not found!</span>
           ) : (
             <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
               <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                {allJobs.length !== 0 ? allJobs.map((job) => (
+                {filterJobs.length !== 0 ? filterJobs.map((job) => (
                   <div className="w-full" key={job._id}>
                     <Job  job={job}/>
                   </div>
--- frontend/src/components/shared/FilterCard.jsx
+++ frontend/src/components/shared/FilterCard.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group'
 import { Label } from '../ui/label'
+import { useDispatch } from 'react-redux'
+import { setSearchedQuery } from '@/redux/slices/jobSlice'
 
 const fitlerData = [
     {
@@ -11,28 +13,33 @@ const fitlerData = [
         fitlerType: "Industry",
         array: ["Frontend Developer", "Backend Developer", "FullStack Developer"]
     },
-    {
-        fitlerType: "Salary",
-        array: ["0-40k", "42-1lakh", "1lakh to 5lakh"]
-    },
 ]
 
 const FilterCard = () => {
+    const [selectedValue, setSelectedValue] = useState('');
+    const dispatch = useDispatch()
+    const handleChange= (value) => {
+        setSelectedValue(value)
+    }
+    useEffect(() => {
+        dispatch(setSearchedQuery(selectedValue))
+    }, [selectedValue])
   return (
     <div className='p-3 w-full bg-white rounded-md'>
         <h1 className='font-bold text-lg'>Filter Jobs</h1>
         <hr className='mt-3'/>
-        <RadioGroup>
+        <RadioGroup value={selectedValue} onValueChange={handleChange}>
             {
                 fitlerData.map((data, index) => (
                     <div className="flex gap-1 sm:block" key={index}>
                         <h1 className='text-sm sm:text-xl sm:font-bold'>{data.fitlerType}</h1>
                         {
-                            data.array.map((item, index) => {
+                            data.array.map((item, idx) => {
+                                const itemId = `r${index} - ${idx}`
                                 return (
-                                    <div className="flex items-center space-x-2 my-2">
-                                        <RadioGroupItem className="flex" value={item} />
-                                        <Label className="text-xs sm:text-base">{item}</Label>
+                                    <div className="flex items-center space-x-2 my-2" key={idx}>
+                                        <RadioGroupItem className="flex" value={item} id={itemId}/>
+                                        <Label htmlFor={itemId} className="text-xs sm:text-base">{item}</Label>
                                     </div>
                                 )
                             })
